1e408fcb90f10d4f2c2b9d6933a82666616a1496
srikanthraj9 <srikanthuppalapati09@gmail.com>
2025-06-11 10:10:50 +0530

    Added new Feature:added password search

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-print(data.to_dict())
 new_dict = {row.letter:row.code for (index,row) in data.iterrows()}
 print(new_dict)
-word = input("enter a word: ").upper()
-out_list = [new_dict[letter] for letter in word]
-print(out_list)
+
+def generate_phonetic():
+    word = input("enter a word: ").upper()
+    try:
+        out_list = [new_dict[letter] for letter in word]
+    except KeyError:
+        print("sorry only letters")
+        generate_phonetic()
+    else:
+        print(out_list)
+
+generate_phonetic()
